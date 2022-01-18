3fba86d9ad608bd07f0c88b2cb58edbc6aa81760
Arthur_Lebedev <IinfernalI21@gmail.com>
2022-01-18 17:45:40 +0200

    fix_doubleMusic

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -7,6 +7,18 @@ public class Music : MonoBehaviour
 {
     private void Awake()
     {
+        int numMusic = FindObjectsOfType<Music>().Length;
         DontDestroyOnLoad(gameObject);
+        if (numMusic > 1)
+        {
+            Destroy(gameObject);
+        }
+        /*if (FindObjectsOfType<Music>().Length > 0)
+        {
+            for (int i = FindObjectsOfType<Music>().Length; i > 1; i--)
+            {
+                Destroy(gameObject);
+            }
+        }*/
     }
 }
