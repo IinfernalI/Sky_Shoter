b38d2e9ec84046bafe99da20bf238d20d54ffdfe
Arthur_Lebedev <IinfernalI21@gmail.com>
2022-01-21 17:05:20 +0200

    Add_HealthToEnemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] private GameObject deathFX;
     [SerializeField] private Transform parent;
     [SerializeField] private int scorePerHit = 10;
+    [SerializeField] private int hits = 20;
+    [SerializeField] private int damage = 10;
 
     private Score scoreObj;
     
@@ -43,13 +45,22 @@ public class Enemy : MonoBehaviour
     
     private void OnParticleCollision(GameObject other)
     {
+        scoreObj.ScoreHit(scorePerHit);
+        print("hit" + gameObject.name);
+        hits -= damage;
+        if (hits <= 0 )
+        {
+            Death();Death();
+        }
+    }
+
+    void Death()
+    {
         //берем заранее прикрепленный префаб взрыва в deathFX и переносим его в новый обьект GameObject
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity); //взрыв от обьекта
         fx.transform.parent = parent; //Присваеваем новый обьект в обьект родителя для удаления муссора
-        
-        scoreObj.ScoreHit(scorePerHit);
-        
-        Destroy(gameObject); 
-        print("Kill" + gameObject.name);
+
+        Destroy(gameObject);
+        print("Dead" + gameObject.name);
     }
 }
